442e0ebe2119c1124b0888ca8f3127327ffea608
onvlt <ondrej@nyv.lt>
2026-09-05 13:53:52 +0200

    refactor

--- flake.nix
+++ flake.nix
@@ -5,37 +5,57 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { nixpkgs, ... }:
     let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
       archMap = {
-        x86_64-linux = "x64";
-        aarch64-linux = "arm64";
+        x86_64-linux = {
+          suffix = "x64";
+          sha256 = "187glnyzhv6aigxpnicvbxv30jshp7ygwikkbaci5xpfb61w4sf1";
+        };
+        aarch64-linux = {
+          suffix = "arm64";
+          sha256 = "0mhb9xqzb0qpy46yh5wrrixw1zhdvnzknr0yrl9cf7qhp09jls64";
+        };
       };
 
-      makePackage = system:
+      mkPackage =
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
-          appimageTools = pkgs.appimageTools;
-
+          arch = archMap.${system};
           pname = "melia";
           version = "1.1.370";
-          arch = archMap.${system};
-          url = "https://github.com/buxjr311/melia-app/releases/download/v${version}/${pname}_${version}_${arch}.AppImage";
-          sha256 = if system == "x86_64-linux" then "187glnyzhv6aigxpnicvbxv30jshp7ygwikkbaci5xpfb61w4sf1" else "0mhb9xqzb0qpy46yh5wrrixw1zhdvnzknr0yrl9cf7qhp09jls64";
-
-        in
-        appimageTools.wrapType2 {
-          inherit pname version;
           src = pkgs.fetchurl {
-            inherit url sha256;
+            url = "https://github.com/buxjr311/melia-app/releases/download/v${version}/${pname}_${version}_${arch.suffix}.AppImage";
+            inherit (arch) sha256;
           };
+        in
+        pkgs.appimageTools.wrapType2 {
+          inherit pname version src;
+
           extraPkgs = pkgs: [
             pkgs.libappimage
           ];
         };
     in
     {
-      packages.x86_64-linux.melia = makePackage "x86_64-linux";
-      packages.aarch64-linux.melia = makePackage "aarch64-linux";
+      packages = forAllSystems (
+        system:
+        let
+          package = mkPackage system;
+        in
+        {
+          default = package;
+          melia = package;
+        }
+      );
     };
 }
